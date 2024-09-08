f22552d34843c9a4b4bedca500db5de2d23c92a3
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-09-08 17:29:36 +0700

    Create grid and append in container
    Create grid in javascript and append into container div.
    Adjust the CSS property to account for wrap in order to create cells.

--- javascript.js
+++ javascript.js
@@ -3,7 +3,21 @@ const gridRow = 16;
 const gridColumn = 16;
 
 const container = document.createElement("div");
-container.classList.add("flexcontainer");
+container.classList.add("flexcontainer")
 container.style.width = `${gridSize}px`;
 container.style.height = `${gridSize}px`;
 document.body.appendChild(container);
+
+function createGrid() {
+    for (let i = 0; i < (gridRow * gridColumn); i++) {
+        const gridCell = document.createElement("div");
+
+        gridCell.style.width = `${(gridSize / gridColumn) - 2}px`;
+        gridCell.style.height = `${(gridSize / gridRow) - 2}px`;
+        gridCell.classList.add("cell");
+
+        container.appendChild(gridCell);
+    }
+}
+
+createGrid();
