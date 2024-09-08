5f952f2c214355e617fb86b195d867345d489ad4
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-09-08 20:42:25 +0700

    Debug Prompt Size button
    Fix the logic of the prompt size button, allowing it to remove and create the container div everytime the button is pressed.

--- javascript.js
+++ javascript.js
@@ -12,24 +12,26 @@ promptBtn.classList.add("promptbutton");
 promptBtn.textContent = "Prompt Size";
 buttonDiv.appendChild(promptBtn);
 
+let container;
+
 promptBtn.addEventListener("click", () => {
     const gridSizeInput = prompt("How many squares per sides would you like?");
     if (gridSizeInput > 100) {
         prompt("The inputted number is too high, please input a number lower than or equal to 100.")
     }
+
+    if (container){
+        container.remove();
+    };
+
+    container = document.createElement("div");
+    container.classList.add("flexcontainer")
+    container.style.width = `${gridSize}px`;
+    container.style.height = `${gridSize}px`;
+    document.body.appendChild(container);
+
     const newGridRow = gridSizeInput;
     const newGridColumn = gridSizeInput;
-    
-    console.log(newGridRow);
-    console.log(newGridColumn);
-
-    container.remove();
-
-    const newContainer = document.createElement("div");
-    newContainer.classList.add("flexcontainer")
-    newContainer.style.width = `${gridSize}px`;
-    newContainer.style.height = `${gridSize}px`;
-    document.body.appendChild(newContainer);
 
     for (let i = 0; i < (newGridRow * newGridColumn); i++) {
         const gridCell = document.createElement("div");
@@ -38,21 +40,33 @@ promptBtn.addEventListener("click", () => {
         gridCell.style.height = `${(gridSize / newGridRow) - 2}px`;
         gridCell.classList.add("cell");
 
-        newContainer.appendChild(gridCell);
+        container.appendChild(gridCell);
+
+        const randomNumber = Math.floor((Math.random() * 3) + 1);
 
         gridCell.addEventListener("mouseover", () => {
-            gridCell.style.backgroundColor = "blue";
+            if (randomNumber === 1) {
+                gridCell.style.backgroundColor = "red";
+            } else if (randomNumber === 2) {
+                gridCell.style.backgroundColor = "blue";
+            } else if (randomNumber === 3) {
+                gridCell.style.backgroundColor = "green";
+            }
         })
     }  
 })
 
-const container = document.createElement("div");
-container.classList.add("flexcontainer")
-container.style.width = `${gridSize}px`;
-container.style.height = `${gridSize}px`;
-document.body.appendChild(container);
-
 function createGrid() {
+    if (container) {
+        container.remove();
+    }
+    container = document.createElement("div");
+    container.classList.add("flexcontainer")
+    container.style.width = `${gridSize}px`;
+    container.style.height = `${gridSize}px`;
+    document.body.appendChild(container);
+
+
     for (let i = 0; i < (gridRow * gridColumn); i++) {
         const gridCell = document.createElement("div");
 
@@ -63,7 +77,7 @@ function createGrid() {
         container.appendChild(gridCell);
 
         gridCell.addEventListener("mouseover", () => {
-            gridCell.style.backgroundColor = "red";
+            gridCell.style.backgroundColor = "black";
         })
 
     }
