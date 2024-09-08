c9c781846e8ff972145db7cc22b3edd6fd0bf4de
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-09-08 19:31:44 +0700

    Create a new button to adjust the size of the grid
    Create a new function to remove old container, add new container, and add new gridcell according to the number of prompt

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-const gridSize = 600;
-const gridRow = 16;
-const gridColumn = 16;
+let gridSize = 600;
+let gridRow = 16;
+let gridColumn = 16;
 
 const buttonDiv = document.createElement("div");
 buttonDiv.classList.add("buttondiv");
@@ -11,6 +11,37 @@ const promptBtn = document.createElement("button");
 promptBtn.classList.add("promptbutton");
 promptBtn.textContent = "Prompt Size";
 buttonDiv.appendChild(promptBtn);
+
+promptBtn.addEventListener("click", () => {
+    const gridSizeInput = prompt("How many squares per sides would you like?");
+    const newGridRow = gridSizeInput;
+    const newGridColumn = gridSizeInput;
+    
+    console.log(newGridRow);
+    console.log(newGridColumn);
+
+    container.remove();
+
+    const newContainer = document.createElement("div");
+    newContainer.classList.add("flexcontainer")
+    newContainer.style.width = `${gridSize}px`;
+    newContainer.style.height = `${gridSize}px`;
+    document.body.appendChild(newContainer);
+
+    for (let i = 0; i < (newGridRow * newGridColumn); i++) {
+        const gridCell = document.createElement("div");
+
+        gridCell.style.width = `${(gridSize / newGridColumn) - 2}px`;
+        gridCell.style.height = `${(gridSize / newGridRow) - 2}px`;
+        gridCell.classList.add("cell");
+
+        newContainer.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover", () => {
+            gridCell.style.backgroundColor = "blue";
+        })
+    }  
+})
 
 const container = document.createElement("div");
 container.classList.add("flexcontainer")
