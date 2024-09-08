28daf9366096790008ea81611d66ef3f29f3fb34
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-09-08 18:43:37 +0700

    Create div to contain menu button
    Create div for menu button.
    Create prompt button and append it within the menu div

--- javascript.js
+++ javascript.js
@@ -2,6 +2,16 @@ const gridSize = 600;
 const gridRow = 16;
 const gridColumn = 16;
 
+const buttonDiv = document.createElement("div");
+buttonDiv.classList.add("buttondiv");
+buttonDiv.style.width = `${gridSize}px`;
+document.body.appendChild(buttonDiv);
+
+const promptBtn = document.createElement("button");
+promptBtn.classList.add("promptbutton");
+promptBtn.textContent = "Prompt Size";
+buttonDiv.appendChild(promptBtn);
+
 const container = document.createElement("div");
 container.classList.add("flexcontainer")
 container.style.width = `${gridSize}px`;
